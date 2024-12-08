66d131a2c693b8af306179ec493bd97dc73bcb30
IloveGreeny <greenxd23@gmail.com>
2024-12-08 20:19:25 +0400

    message

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -71,8 +71,8 @@ export default function Products() {
                     </div>
                 ))}
             </div>
-            <div className="mt-8 mr-3">
-                <Link to="/favourites" className="mt-8 px-6 py-2 bg-indigo-600 text-white font-semibold rounded-lg shadow hover:bg-indigo-700 transition-all">
+            <div className="mt-8">
+                <Link to="/favourites" className="mt-8 px-6 py-2 bg-indigo-600 text-white font-semibold rounded-lg shadow hover:bg-indigo-700 transition-all mr-3">
                     View Favourites
                 </Link>
                 <Link to="/create-product" className="mt-8 px-6 py-2 bg-indigo-600 text-white font-semibold rounded-lg shadow hover:bg-indigo-700 transition-all">
